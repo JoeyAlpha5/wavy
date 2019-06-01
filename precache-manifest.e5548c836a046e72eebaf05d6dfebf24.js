9b5578134dcf84c9e8c8fa06e5342ed4c3643472
JoeyAlpha5 <35291886+JoeyAlpha5@users.noreply.github.com>
2019-06-01 02:34:46 +0200

    initial commit

---
+++ precache-manifest.e5548c836a046e72eebaf05d6dfebf24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8ab776eeaf11abeedbc0e5dac29dbe",
+    "url": "/wavy/index.html"
+  },
+  {
+    "revision": "784ed3d3d993f7e36d26",
+    "url": "/wavy/static/css/main.fb88ba6b.chunk.css"
+  },
+  {
+    "revision": "39e43a7e166f4955a3a0",
+    "url": "/wavy/static/js/2.ae777650.chunk.js"
+  },
+  {
+    "revision": "784ed3d3d993f7e36d26",
+    "url": "/wavy/static/js/main.873005dc.chunk.js"
+  },
+  {
+    "revision": "4a84ef97b69493e2a6c1",
+    "url": "/wavy/static/js/runtime~main.78153525.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wavy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8ba53b1f369dc0fb87faa26d2df9b79",
+    "url": "/wavy/static/media/wavy.a8ba53b1.JPG"
+  }
+]);
